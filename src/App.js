82203d89a2a27174b6bb5b4fde44fe791fbac9ff
Eduardo Reche <edu.reche@gmail.com>
2017-07-01 18:18:02 -0300

    Auth reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -15,18 +15,14 @@ class App extends Component {
       storageBucket: 'course-manager-b16b6.appspot.com',
       messagingSenderId: '714551409825'
     };
-    
+
     firebase.initializeApp(config);
   }
 
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
