13075151478c63a5c0d3a8ef89b4e00fa04a82fd
Eduardo Reche <edu.reche@gmal.com>
2017-07-01 21:03:13 -0300

    Router and scenes

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeLIst extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+        <Text>Employee List!</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeLIst;
